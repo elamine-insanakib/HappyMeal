0fe6c5e36dfd30206559b630fe6428f1bd7a6b7e
Enzo-Martolini <enzo.martolini@laplateforme.io>
2024-04-09 16:27:33 +0200

    fonction favoris terminé

--- backend/functions.js
+++ backend/functions.js
@@ -1,5 +1,6 @@
 const inputIngredient = document.getElementById('ingredient');
-const favoris = [];
+let favoris = [];
+chargerFavoris();
 
 inputIngredient.addEventListener('input', () => {
     // Récupération de la valeur saisie dans le champ de texte
@@ -44,11 +45,17 @@ async function afficherRecette() {
 
         //Ajout de l'image etoile vide
         imageEtoile = document.createElement("img")
-        imageEtoile.src = "../assets/image/star.empty.png";
-        imageEtoile.width = 20;
+        imageEtoile.src = favoris.includes(index.toString()) ? "../assets/image/star.fill.png" : "../assets/image/star.empty.png";        imageEtoile.width = 20;
         imageEtoile.height = 20;
         recetteDiv.appendChild(imageEtoile)
         recetteDiv.setAttribute("id", index);
+
+        // console.log(favoris)
+        // // Vérifier si la recette est dans les favoris
+        // if (favoris.includes(index.toString()))  {
+        //     console.log('trouvé');
+        //     imageEtoile.src = "../assets/image/star.fill.png"
+        // }
 
         imageEtoile.addEventListener("click", function() {
             ajouterFavoris(this); // Passer l'élément img en tant que paramètre
@@ -74,15 +81,50 @@ async function filtreRecette(nomIngredientRecherche) {
     });
 }
 
-function ajouterFavoris(imageEtoile) {
-    if (imageEtoile.src.includes("star.fill.png")) {
-        imageEtoile.src = "../assets/image/star.empty.png";
-        imageEtoile.classList.remove("favoris");
-    } else {
-        imageEtoile.src = "../assets/image/star.fill.png";
-        imageEtoile.classList.add("favoris");
+
+//Fonctions de favoris
+function sauvegarderFavoris() {
+    localStorage.setItem('favoris', JSON.stringify(favoris));
+}
+
+function chargerFavoris() {
+    const favorisString = localStorage.getItem('favoris');
+    if (favorisString) {
+        favoris = JSON.parse(favorisString);
     }
 }
+
+function ajouterFavoris(imageEtoile) {
+
+    // Récupérer l'élément div parent de l'image étoile, qui contient les informations de la recette
+    const recetteDiv = imageEtoile.parentNode;
+    const recetteIndex = recetteDiv.id;
+    console.log(recetteDiv)
+    console.log(recetteIndex)
+
+    // Vérifier si la recette est déjà dans les favoris
+    const indexDansFavoris = favoris.indexOf(recetteIndex);
+
+    // Si la recette n'est pas dans les favoris, l'ajouter
+    if (indexDansFavoris === -1) {
+        favoris.push(recetteIndex);
+        imageEtoile.src = "../assets/image/star.fill.png"; // changer l'image pour étoile remplie
+        imageEtoile.classList.add("favoris"); // ajouter la classe "favoris" à l'image
+        console.log("push")
+        console.log(favoris)
+        sauvegarderFavoris()
+    } else { // Si la recette est déjà dans les favoris, la retirer
+        favoris.splice(indexDansFavoris, 1);
+        imageEtoile.src = "../assets/image/star.empty.png"; // changer l'image pour étoile vide
+        imageEtoile.classList.remove("favoris"); // retirer la classe "favoris" de l'image
+        console.log("remove")
+        sauvegarderFavoris()
+    }
+
+    // Appel de la fonction pour sauvegarder les favoris dans le localStorage
+    sauvegarderFavoris();
+}
+
 
 
 
